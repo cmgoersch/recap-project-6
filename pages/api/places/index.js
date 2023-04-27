d6083b9e80de20e3386909809ce95719ef5bed32
Carl-M. Goersch <goersch@madways.de>
2023-04-27 17:11:36 +0200

    first steps

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,24 @@
-import { places } from '../../../lib/db';
+import dbConnect from "../../../db/connect";
+import Place from "../../../db/models/Places";
 
-export default function handler(request, response) {
-  return response.status(200).json(places);
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  if (request.method === "GET") {
+    const places = await Place.find();
+    return response.status(200).json(places);
+  }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      const place = new Place(placeData);
+      await place.save();
+      return response.status(201).json({ status: "Place created." });
+    } catch (error) {
+      console.error(error);
+      return response.status(400).json({ error: error.message });
+    }
+  }
 }
